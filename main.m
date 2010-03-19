72e6e3c8e76866151f73548821326fc286f8c61d
Anthony Brummett <brummett@gmail.com>
2010-03-19 13:03:37 -0500

    Initial import of empty project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JewelToy
+//
+//  Created by Anthony Brummett on 3/19/10.
+//  Copyright Washington University School of Medicine 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
